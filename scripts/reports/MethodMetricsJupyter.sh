2fed3647bf25d741f9d1d5654dcd30863d2f31af
JohT <johnnyt@gmx.at>
2023-09-09 21:12:49 +0200

    Split out method metrics into own Jupyter report

--- scripts/reports/MethodMetricsJupyter.sh
+++ scripts/reports/MethodMetricsJupyter.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# Creates the "overview" report (ipynb, md, pdf) based on the Jupyter Notebook "Overview.ipynb".
-# It contains a basic overview on how many Classes, Interfaces, Enums and Annotations earch artifact contains and
-# how they relate to each other.
+# Creates the method metrics report (ipynb, md, pdf) based on the Jupyter Notebook "MethodMetrics.ipynb".
+# It contains effective line counts and cyclomatic complexity of methods per artifact and package
+# and their distribution.
 
 # Requires executeJupyterNotebook.sh
 
@@ -14,20 +14,20 @@ REPORTS_DIRECTORY=${REPORTS_DIRECTORY:-"reports"}
 # CDPATH reduces the scope of the cd command to potentially prevent unintended directory changes.
 # This way non-standard tools like readlink aren't needed.
 REPORTS_SCRIPT_DIR=${REPORTS_SCRIPT_DIR:-$( CDPATH=. cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P )}
-echo "OverviewJupyter: REPORTS_SCRIPT_DIR=${REPORTS_SCRIPT_DIR}"
+echo "MethodMetricsJupyter: REPORTS_SCRIPT_DIR=${REPORTS_SCRIPT_DIR}"
 
 # Get the "scripts" directory by taking the path of this script and going one directory up.
 SCRIPTS_DIR=${SCRIPTS_DIR:-"${REPORTS_SCRIPT_DIR}/.."} # Repository directory containing the shell scripts
-echo "OverviewJupyter: SCRIPTS_DIR=${SCRIPTS_DIR}"
+echo "MethodMetricsJupyter: SCRIPTS_DIR=${SCRIPTS_DIR}"
 
 # Get the "jupyter" directory by taking the path of this script and going two directory up and then to "jupyter".
 JUPYTER_NOTEBOOK_DIRECTORY=${JUPYTER_NOTEBOOK_DIRECTORY:-"${SCRIPTS_DIR}/../jupyter"} # Repository directory containing the Jupyter Notebooks
-echo "OverviewJupyter: JUPYTER_NOTEBOOK_DIRECTORY=$JUPYTER_NOTEBOOK_DIRECTORY"
+echo "MethodMetricsJupyter: JUPYTER_NOTEBOOK_DIRECTORY=$JUPYTER_NOTEBOOK_DIRECTORY"
 
 # Create report directory
-REPORT_NAME="overview"
+REPORT_NAME="method-metrics"
 FULL_REPORT_DIRECTORY="${REPORTS_DIRECTORY}/${REPORT_NAME}"
 mkdir -p "${FULL_REPORT_DIRECTORY}"
 
 # Execute and convert the following Jupyter Notebook within the given reports directory
-(cd "${FULL_REPORT_DIRECTORY}" && exec ${SCRIPTS_DIR}/executeJupyterNotebook.sh ${JUPYTER_NOTEBOOK_DIRECTORY}/Overview.ipynb) || exit 1
+(cd "${FULL_REPORT_DIRECTORY}" && exec ${SCRIPTS_DIR}/executeJupyterNotebook.sh ${JUPYTER_NOTEBOOK_DIRECTORY}/MethodMetrics.ipynb) || exit 1
