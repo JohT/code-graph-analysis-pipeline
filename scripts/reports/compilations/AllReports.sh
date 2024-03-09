f02e9c6c10b46ae13359228812c3034502c896c8
JohT <johnnyt@gmx.at>
2024-03-09 10:29:35 +0100

    Add further considerations regarding coupling

--- scripts/reports/compilations/AllReports.sh
+++ scripts/reports/compilations/AllReports.sh
@@ -15,10 +15,14 @@ set -o errexit -o pipefail
 REPORT_COMPILATIONS_SCRIPT_DIR=${REPORT_COMPILATIONS_SCRIPT_DIR:-$( CDPATH=. cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P )}
 echo "AllReports: REPORT_COMPILATIONS_SCRIPT_DIR=${REPORT_COMPILATIONS_SCRIPT_DIR}"
 
-# The reports will not be generically searched as files anymore
-# but will be processed in order. Especially the visualization
+# The reports will not be generically searched as files anymore.
+# Instead, they will be processed in order. Especially the visualization
 # needs to be done as a last step to be able to use properties
 # and data written to the Graph in the CsvReports.
+# Additionally, Jupyter notebooks can then use precalculated properties from
+# the CSV reports. The drawback of this approach is that the introduced
+# dependencies take away the self-containment of those reports and make it
+# then hard to parallelize them. So if coupling can be prevented, it still should.
 source "${REPORT_COMPILATIONS_SCRIPT_DIR}/CsvReports.sh"
 source "${REPORT_COMPILATIONS_SCRIPT_DIR}/JupyterReports.sh"
 source "${REPORT_COMPILATIONS_SCRIPT_DIR}/VisualizationReports.sh"
