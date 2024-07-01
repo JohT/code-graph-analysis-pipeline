3f8b24b616cb95169d8e10a4eea727eea523c777
JohT <7671054+JohT@users.noreply.github.com>
2024-07-01 19:56:19 +0200

    Query interface segregation candidates for Typescript

--- scripts/reports/InternalDependenciesCsv.sh
+++ scripts/reports/InternalDependenciesCsv.sh
@@ -49,6 +49,7 @@ execute_cypher "${CYCLIC_DEPENDENCIES_CYPHER_DIR}/Cyclic_Dependencies_between_Ar
 execute_cypher "${CYCLIC_DEPENDENCIES_CYPHER_DIR}/Cyclic_Dependencies_for_Typescript.cypher" > "${FULL_REPORT_DIRECTORY}/Cyclic_Dependencies_for_Typescript.csv"
 
 execute_cypher "${INTERNAL_DEPENDENCIES_CYPHER_DIR}/Candidates_for_Interface_Segregation.cypher" > "${FULL_REPORT_DIRECTORY}/InterfaceSegregationCandidates.csv"
+execute_cypher "${INTERNAL_DEPENDENCIES_CYPHER_DIR}/Candidates_for_Interface_Segregation_for_Typescript.cypher" > "${FULL_REPORT_DIRECTORY}/Candidates_for_Interface_Segregation_for_Typescript.csv"
 
 execute_cypher "${INTERNAL_DEPENDENCIES_CYPHER_DIR}/List_all_Java_artifacts.cypher" > "${FULL_REPORT_DIRECTORY}/List_all_Java_artifacts.csv"
 execute_cypher "${INTERNAL_DEPENDENCIES_CYPHER_DIR}/List_types_that_are_used_by_many_different_packages.cypher" > "${FULL_REPORT_DIRECTORY}/WidelyUsedTypes.csv"
