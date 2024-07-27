17f75eb062c647dec9fcb3eb34e260cc9faf49cb
JohT <7671054+JohT@users.noreply.github.com>
2024-07-27 20:31:47 +0200

    Fix npm package.json scan error by removing the author.
    Workaround can be removed when https://github.com/jqassistant-plugin/jqassistant-npm-plugin/issues/5 is fixed.

--- scripts/copyPackageJsonFiles.sh
+++ scripts/copyPackageJsonFiles.sh
@@ -3,6 +3,8 @@
 # Copies all package.json files inside the source directory into the artifacts/npm-package-json directory.
 # It retains the original folder structure where the package.json files were in.
 
+# This script "jq" ( https://stedolan.github.io/jq ).
+
 # Fail on any error ("-e" = exit on first error, "-o pipefail" exist on errors within piped commands)
 set -o errexit -o pipefail
 
@@ -21,6 +23,7 @@ fi
     cd "./${SOURCE_DIRECTORY}"
     
     echo "copyPackageJsonFiles: Existing package.json files will be copied from from ${SOURCE_DIRECTORY} to ../${ARTIFACTS_DIRECTORY}/${NPM_PACKAGE_JSON_ARTIFACTS_DIRECTORY}"
+    echo "copyPackageJsonFiles: Author will be removed as workaround for https://github.com/jqassistant-plugin/jqassistant-npm-plugin/issues/5"
     
     for file in $( find . -path ./node_modules -prune -o -name 'package.json' -print0 | xargs -0 -r -I {}); do
         fileDirectory=$(dirname "${file}")
@@ -29,5 +32,11 @@ fi
 
         mkdir -p "${targetDirectory}"
         cp "${file}" "${targetDirectory}"
+
+        # Workaround until the following issue is resolved:
+        # https://github.com/jqassistant-plugin/jqassistant-npm-plugin/issues/5
+        fileName=$(basename "${file}")
+        jq 'del(.author)' "${targetDirectory}/${fileName}" > "${targetDirectory}/${fileName}.edited"
+        jq 'del(.contributors)' "${targetDirectory}/${fileName}.edited" > "${targetDirectory}/${fileName}"
     done
 )
