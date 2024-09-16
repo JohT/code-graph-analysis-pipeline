f38ccd0c5eacbd1d6db660cca02808454a0ccdc2
JohT <7671054+JohT@users.noreply.github.com>
2024-09-16 17:28:56 +0200

    Run Typescript scan in workspace and  pass scan directory

--- scripts/scanTypescript.sh
+++ scripts/scanTypescript.sh
@@ -46,14 +46,12 @@ else
 
     for directory in ${source_directories}; do
         directory_name=$(basename "${directory}");
-        # Note: The npx command will be executed in the source directory using a subshell by putting parentheses around it.
-        #       The subshell is the reason why it isn't needed to change back to the main directory after execution.
         # Note: This script must not output anything except for the return code to stdout,
         #       all output of the scanning needs to be redirected to stderr using ">&2".
         #       For later troubleshooting, the output is also copied to a dedicated log file using "tee".
         # Note: Don't worry about the hardcoded version number. It will be updated by Renovate using a custom Manager.
         # Note: NODE_OPTIONS --max-old-space-size=4096 increases the memory for larger projects to scan
-        ( cd "${directory}" && NODE_OPTIONS="${NODE_OPTIONS} --max-old-space-size=4096" npx --yes @jqassistant/ts-lce@1.3.0 --extension React 2>&1 | tee "${LOG_DIRECTORY}/jqassistant-typescript-scan-${directory_name}.log" >&2 || exit )
+        NODE_OPTIONS="${NODE_OPTIONS} --max-old-space-size=4096" npx --yes @jqassistant/ts-lce@1.3.0 "${directory}" --extension React 2>&1 | tee "${LOG_DIRECTORY}/jqassistant-typescript-scan-${directory_name}.log" >&2
         processed_directories=$((processed_directories + 1))
         echo "" >&2
         echo "scanTypescript: Scanned ${directory_name} (${processed_directories}/${total_directories}) ----------------------" >&2
